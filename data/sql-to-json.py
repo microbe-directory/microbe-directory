e4e89ecb6e3cba4ec1161c0d35e44dcffb818734
David Westfall <david@reflashed.com>
2017-11-20 21:17:18 -0500

    Fixed db to match metaphlan.\nEssentially changed some spellings in our db and added a few streptomyces species

--- data/sql-to-json.py
+++ data/sql-to-json.py
@@ -13,5 +13,8 @@ columns = next(zip(*cursor.description))
  # row[7] is species name
  # replace ' ' with '_' (consistent w/ metaphlan)
 data = {row[7].replace(' ', '_'): dict(zip(columns, row)) for row in rows}
+
+#print(data['Ipomoea_begomovirus_satellite_DNA_beta'])
+
 with open('microbe-directory.json', 'w') as file:
-    file.write(json.dumps(data))
+    file.write(json.dumps(data).replace('N/A', 'A')) # replace is bc json.dumps is esacping this for some reason
